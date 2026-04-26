ed2be1329c035f250569f20605f02e67e23e3671
adityanarayan8383-ai <adityanarayan8383@gmail.com>
2026-04-26 14:26:12 +0530

    Update SPS.py

--- SPS.py
+++ SPS.py
@@ -1,20 +1,13 @@
 import random
-
 print("STONE PAPER SCISSOR GAME")
-
 choices = ["stone", "paper", "scissor"]
-
 while True:
     user = input("\nEnter your choice (stone/paper/scissor): ").lower()
-    
     if user not in choices:
         print("Invalid choice! Try again.")
         continue
-    
     computer = random.choice(choices)
-    
     print("Computer chose:", computer)
-
     if user == computer:
         print("It's a DRAW!")
     elif (user == "stone" and computer == "scissor") or \
@@ -23,7 +16,6 @@ while True:
         print("You WIN!")
     else:
         print("You LOSE!")
-
     again = input("Play again? (yes/no): ").lower()
     if again != "yes":
         print("Game Over!")
